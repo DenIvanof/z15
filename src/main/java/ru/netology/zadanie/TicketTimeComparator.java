8d6fc1df339ded65b0a7d383835ec374a4343d2c
Ivanich <deniska090798@gmail.com>
2024-02-06 22:30:54 +0400

    first commit

---
+++ src/main/java/ru/netology/zadanie/TicketTimeComparator.java
@@ -0,0 +1,14 @@
+package ru.netology.zadanie;
+
+import java.util.Comparator;
+
+public class TicketTimeComparator implements Comparator<Ticket> {
+
+    @Override
+    public int compare(Ticket t1, Ticket t2) {
+        // ваш код
+        int comparison1 = t1.getTimeTo() - t1.getTimeFrom();
+        int comparison2 = t2.getTimeTo() - t2.getTimeFrom();
+        return integer.compare(comparison1, comparison2);
+    }
+}
